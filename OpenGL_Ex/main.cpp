59f208557cab50166cd08d02be7d1c68ca4ba7db
bo.li <bo.li@100class.com>
2021-12-09 13:03:47 +0800

    Initial Commit

---
+++ OpenGL_Ex/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Ex
+//
+//  Created by Plum on 2021/12/9.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
