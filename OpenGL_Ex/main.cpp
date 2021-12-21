dab46a88d2735bd1e52e541b02d836fc277253b0
bo.li <bo.li@100class.com>
2021-12-21 11:20:31 +0800

    feat: 【你好三角形】绘制矩形

--- OpenGL_Ex/main.cpp
+++ OpenGL_Ex/main.cpp
@@ -95,23 +95,36 @@ int main(int argc, const char * argv[]) {
     
     
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,  //左上角
+        -0.5f, -0.5f, 0.0f, //左下角
+        0.5f, 0.5f, 0.0f,   //右上角
+        0.5f, -0.5f, 0.0f,  //右下角
     };
-    unsigned int VBO, VAO;
+    
+    unsigned int indices[] = {
+        0, 1, 3,    //第一个三角形
+        0, 2, 3,    //第二个三角形
+    };
+    
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -123,7 +136,7 @@ int main(int argc, const char * argv[]) {
         // draw
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -131,6 +144,7 @@ int main(int argc, const char * argv[]) {
     
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
     
     glfwTerminate();
