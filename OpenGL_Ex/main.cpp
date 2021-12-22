374c3a270aabbb49ed12818d6d808c92fa4df9cb
bo.li <bo.li@100class.com>
2021-12-22 10:41:35 +0800

    feat:【着色器】顶点附带颜色数据

--- OpenGL_Ex/main.cpp
+++ OpenGL_Ex/main.cpp
@@ -16,17 +16,20 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 
 const char *vertexShaderSource = "#version 410 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 courColor;\n"
+    "out vec3 vertexColor;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   vertexColor = courColor;\n"
     "}\0";
 
 const char *fragmentShaderSource = "#version 410 core\n"
+    "in vec3 vertexColor;\n"
     "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = ourColor;\n"
+    "   FragColor = vec4(vertexColor, 1.0);\n"
     "}\0";
 
 int main(int argc, const char * argv[]) {
@@ -97,31 +100,33 @@ int main(int argc, const char * argv[]) {
     
     
     float vertices[] = {
-        -0.5f, 0.5f, 0.0f,  //左上角
-        -0.5f, -0.5f, 0.0f, //左下角
-        0.5f, 0.5f, 0.0f,   //右上角
-        0.5f, -0.5f, 0.0f,  //右下角
+        0.0f, 0.5f, 0.0f,
+        1.0f, 0.0f, 0.0f,   // 颜色红
+        -0.5f, -0.5f, 0.0f,
+        0.0f, 1.0f, 0.0f,   // 颜色绿
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.0f, 1.0f,   // 颜色蓝
     };
     
-    unsigned int indices[] = {
-        0, 1, 3,    //第一个三角形
-        0, 2, 3,    //第二个三角形
-    };
     
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
+//    unsigned int EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+//    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
     
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
+    
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -145,7 +150,7 @@ int main(int argc, const char * argv[]) {
         glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -153,7 +158,7 @@ int main(int argc, const char * argv[]) {
     
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+//    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
     
     glfwTerminate();
