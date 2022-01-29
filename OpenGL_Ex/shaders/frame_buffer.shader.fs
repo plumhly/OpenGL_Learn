640bd9bf092126029d50989bf8b969fbda7dcc1a
bo.li <bo.li@100class.com>
2022-01-29 16:22:47 +0800

    feat:【帧缓存】反相

--- OpenGL_Ex/shaders/frame_buffer.shader.fs
+++ OpenGL_Ex/shaders/frame_buffer.shader.fs
@@ -6,5 +6,5 @@ uniform sampler2D texture1;
 out vec4 fragColor;
 
 void main() {
-    fragColor = texture(texture1, texCoords);
+    fragColor = vec4(vec3(1.0 - texture(texture1, texCoords)), 1.0);
 }
