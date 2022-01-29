0b25e30fb7e8969c93d998b123e057d2a522d43a
bo.li <bo.li@100class.com>
2022-01-29 16:25:13 +0800

    feat:【帧缓存】灰化

--- OpenGL_Ex/shaders/frame_buffer.shader.fs
+++ OpenGL_Ex/shaders/frame_buffer.shader.fs
@@ -6,5 +6,7 @@ uniform sampler2D texture1;
 out vec4 fragColor;
 
 void main() {
-    fragColor = vec4(vec3(1.0 - texture(texture1, texCoords)), 1.0);
+    fragColor = texture(texture1, texCoords);
+    float average = (fragColor.r + fragColor.g + fragColor.b) / 3.0;
+    fragColor = vec4(average, average, average, 1.0);
 }
