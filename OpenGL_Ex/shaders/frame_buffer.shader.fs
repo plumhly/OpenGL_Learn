9fdd91dff88ae5904cd58d44bfb37f8eefd167cf
bo.li <bo.li@100class.com>
2022-01-29 16:37:36 +0800

    feat:【帧缓存】核效果

--- OpenGL_Ex/shaders/frame_buffer.shader.fs
+++ OpenGL_Ex/shaders/frame_buffer.shader.fs
@@ -5,8 +5,36 @@ uniform sampler2D texture1;
 
 out vec4 fragColor;
 
+const float offset = 1.0 / 300.0;
+
 void main() {
-    fragColor = texture(texture1, texCoords);
-    float average = 0.2126 * fragColor.r + 0.7152 * fragColor.g + 0.0722 * fragColor.b;
-    fragColor = vec4(average, average, average, 1.0);
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // 左上
+        vec2( 0.0f,    offset), // 正上
+        vec2( offset,  offset), // 右上
+        vec2(-offset,  0.0f),   // 左
+        vec2( 0.0f,    0.0f),   // 中
+        vec2( offset,  0.0f),   // 右
+        vec2(-offset, -offset), // 左下
+        vec2( 0.0f,   -offset), // 正下
+        vec2( offset, -offset)  // 右下
+    );
+
+    float kernel[9] = float[](
+        -1, -1, -1,
+        -1,  9, -1,
+        -1, -1, -1
+    );
+
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++){
+        sampleTex[i] = vec3(texture(texture1, texCoords.st + offsets[i]));
+    }
+
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++){
+        col += sampleTex[i] * kernel[i];
+    }
+
+    fragColor = vec4(col, 1.0);
 }
